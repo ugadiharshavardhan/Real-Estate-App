0deda6a891d1227a4326025da2e31832baa7b9dd
harsha7075226822 <ugadiharshavardhan@gmail.com>
2026-03-09 10:49:26 +0530

    chore: Downgrade Next.js, disable React Compiler, and broaden remote image path patterns in the configuration.

--- next.config.mjs
+++ next.config.mjs
@@ -1,44 +1,53 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  reactCompiler: true,
+  reactCompiler: false,
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "images.unsplash.com",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "c.pxhere.com",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "upload.wikimedia.org",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "www.sakshi.com",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "s7ap1.scene7.com",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "media.licdn.com",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "framerusercontent.com",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "rmgoe.org",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "i.pravatar.cc",
+        pathname: "/**",
       },
     ],
   },
